b00cc629cc212d6848e3853acb37bdccc6fc71ee
Maya <maya.kolomoetc@yandex.ru>
2025-04-18 22:58:03 +0300

    first commit

---
+++ components/MostPopular.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="container mt-8">
+		<h1 class="text-[40px] font-nunito font-bold"><span class="text-amber-600 mx-3">|</span> Самые популярные</h1>
+		<div class="flex gap-10">
+			<!-- здесь карточки в отдельный компонент вынесу и для каждой v-for будет для рендера содержимого -->
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+		</div>	
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { Star } from 'lucide-vue-next'
+	
+</script>
---
+++ components/Recomendation.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="container mt-20">
+		<h1 class="text-[40px] font-nunito font-bold">Рекомендовано</h1>
+			<div class="flex gap-10">
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg class="w-[215px] h-[215px]" format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg class="w-[215px] h-[215px]" format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg class="w-[215px] h-[215px]" format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+				<div class="w-64 mt-8">
+					<div class="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+						<NuxtImg class="w-[215px] h-[215px]" format="webp" src="/bear.png" alt="Logo" />
+					</div>
+					<div class="mt-4 flex gap-2">
+						<Star color="#f3da0b" fill="#f3da0b" class="w-5" />
+						<p>8.9</p>
+					</div>
+					<p class="mt-2 mb-4 text-2xl">Зеленый кайф</p>
+					<div class="flex justify-between items-center mt-4">
+						<Button variant="default" class="text-base w-30 font-bold">
+							Оценить
+						</Button>
+						<Button variant="outline" class="text-base w-30 font-bold">
+							Любимое
+						</Button>
+					</div>
+				</div>
+			</div>	
+	</div>
+</template>
+<script setup lang="ts">
+	import { Star } from 'lucide-vue-next'
+</script>
